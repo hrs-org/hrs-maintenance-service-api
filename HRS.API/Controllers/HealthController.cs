4541e1384568fdad4ff32e5c7857d75e32172544
feri-nus-iss <e1546606@u.nus.edu>
2025-10-20 23:50:54 +0800

    [NO-REF] Fix Health

--- HRS.API/Controllers/HealthController.cs
+++ HRS.API/Controllers/HealthController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace HRS.API.Controllers;
 
 [ApiController]
-[Route("api/health")]
+[Route("health")]
 public class HealthController : ControllerBase
 {
     [HttpGet]
