dfaae5fe6f0bc1ab047ff8223a5aeffa2c976e10
EvanQwQ-CMD <e1554482@u.nus.edu>
2025-10-21 19:41:15 +0800

    [HRSM-18] Add-Jwt-Methods

--- HRS.API/Program.cs
+++ HRS.API/Program.cs
@@ -4,6 +4,7 @@ using HRS.API.Filters;
 using HRS.API.Middleware;
 using HRS.API.Services;
 using HRS.API.Services.Interfaces;
+using HRS.API.Handlers;
 using HRS.API.Validators.Maintenance;
 using HRS.Domain.Interfaces;
 using HRS.Infrastructure.Repositories;
@@ -21,6 +22,18 @@ builder.Services.AddScoped<IUserContextService, UserContextService>();
 
 builder.Services.AddScoped<IItemMaintenanceRepository, ItemMaintenanceRepository>();
 builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddTransient<AuthorizationHeaderHandler>();
+
+builder.Services.AddHttpClient("RentalOrderService", client =>
+{
+    var orderServiceUrl = builder.Configuration["RentalOrderService"]
+        ?? "http://hrs-order-service.orders.svc.cluster.local";
+
+    client.BaseAddress = new Uri(orderServiceUrl);
+    client.Timeout = TimeSpan.FromSeconds(30);
+})
+.AddHttpMessageHandler<AuthorizationHeaderHandler>();
 
 builder.Services.AddControllers(options => { options.Filters.Add<ValidationFilter>(); });
 
